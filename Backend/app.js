181121fecb5297639a9dc81a909de7b6cb59ff27
Tu <tuphan694@gmail.com>
2023-11-04 12:20:01 +0700

    feat: add swagger to backend

--- Backend/app.js
+++ Backend/app.js
@@ -3,16 +3,19 @@ const express = require('express')
 const app = express();
 const path = require('path');
 var bodyParser = require('body-parser')
-const  swaggerJsdoc = require("swagger-jsdoc");
-const  swaggerUi = require("swagger-ui-express");
 const checkToken = require("./src/utils/middleware.js")
 const initWebRoutes = require("./src/routes/index.js");
 const cors = require("cors");
+const swaggerUi = require('swagger-ui-express');
 require("dotenv").config();
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 app.use(cors());
 
 
 app.use('/', express.static(path.join(__dirname, 'lib')))
+
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
 //middleware
