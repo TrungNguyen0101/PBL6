f1938abd49f09a6662045095278719c96233e158
Tu <tuphan694@gmail.com>
2023-11-05 16:49:45 +0700

    feat: test mail

--- Backend/src/models/User.js
+++ Backend/src/models/User.js
@@ -5,15 +5,17 @@ const { ObjectId } = require('mongodb');
 const User = new Schema({
     id: { type: ObjectId },
     username: {
-         type: String,
-         require: true,
-        },
-    phoneNumber : {type: String},
-    email: {type: String,},
-    password: { type: String, require:true},
-    roleID : {
+        type: String,
+        require: true,
+    },
+    phoneNumber: { type: String },
+    email: { type: String, },
+    password: { type: String, require: true },
+    roleID: {
         type: String
-    }
+    },
+    isVerified: { type: Boolean, default: false },
+    verificationCode: { type: String },
 })
 
 module.exports = mongoose.model('User', User);
