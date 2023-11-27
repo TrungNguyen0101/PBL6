3a6660b7400a5dc166d344cc097870fd226d25eb
TrungNguyen0101 <tronzinmj213@gmail.com>
2023-11-27 23:04:57 +0700

    fix GTM

--- FrontEnd/src/app/layout.jsx
+++ FrontEnd/src/app/layout.jsx
@@ -2,6 +2,7 @@
 
 import classNames from 'classnames';
 import { Inter, Montserrat } from 'next/font/google';
+import Script from 'next/script';
 
 import './globals.css';
 import 'react-toastify/dist/ReactToastify.css';
@@ -29,18 +30,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <script>
-          {(function (w, d, s, l, i) {
-            w[l] = w[l] || [];
-            w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-            var f = d.getElementsByTagName(s)[0],
-              j = d.createElement(s),
-              dl = l != 'dataLayer' ? '&l=' + l : '';
-            j.async = true;
-            j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-            f.parentNode.insertBefore(j, f);
-          })(window, document, 'script', 'dataLayer', 'GTM-WV3P6XBH')}
-        </script>
+        <Script>
+          {typeof window !== 'undefined' &&
+            (function (w, d, s, l, i) {
+              w[l] = w[l] || [];
+              w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+              var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+              j.async = true;
+              j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+              f.parentNode.insertBefore(j, f);
+            })(window, document, 'script', 'dataLayer', 'GTM-WV3P6XBH')}
+        </Script>
       </head>
       <body
         className={classNames(
@@ -57,7 +59,7 @@ export default function RootLayout({ children }) {
             src="https://www.googletagmanager.com/ns.html?id=GTM-WV3P6XBH"
             height="0"
             width="0"
-            style="display:none;visibility:hidden"
+            style={{ display: 'none', visibility: 'hidden' }}
           ></iframe>
         </noscript>
       </body>
