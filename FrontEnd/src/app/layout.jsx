dbd989659b148a8842723b40aa3938ede8f6ed20
kyhuyho <hkhuy220802@gmail.com>
2023-11-30 16:10:21 +0700

    fix-header

--- FrontEnd/src/app/layout.jsx
+++ FrontEnd/src/app/layout.jsx
@@ -2,6 +2,7 @@
 
 import classNames from 'classnames';
 import { Inter, Montserrat } from 'next/font/google';
+import Script from 'next/script';
 
 import './globals.css';
 import 'react-toastify/dist/ReactToastify.css';
@@ -28,7 +29,45 @@ const montserrat = Montserrat({
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="dark:bg-slate-900">
+      <head>
+        {/* <Script>
+          {typeof window !== 'undefined' &&
+            (function (w, d, s, l, i) {
+              w[l] = w[l] || [];
+              w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+              var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+              j.async = true;
+              j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+              f.parentNode.insertBefore(j, f);
+            })(window, document, 'script', 'dataLayer', 'GTM-WV3P6XBH')}
+        </Script> */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-Y35FP8XXTL"
+        ></Script>
+        <Script
+          id="clarity_script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-Y35FP8XXTL');
+            `,
+          }}
+        />
+      </head>
+      <body
+        className={classNames(
+          inter.variable,
+          montserrat.variable,
+          'flex flex-col min-h-screen'
+        )}
+      >
         <Provider store={store}>{children}</Provider>
         <ToastContainer autoClose={1000} />
         <Backtop />
@@ -36,8 +75,3 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
-/* className={classNames(
-          inter.variable,
-          montserrat.variable,
-          'flex flex-col min-h-screen'
-        )} */
