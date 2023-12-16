c4a59a4f92d8f5e4a9596dd1bf555d95c593c2eb
TrungNguyen0101 <tronzinmj213@gmail.com>
2023-12-16 17:00:14 +0700

    update nguyen

--- FrontEnd/src/services/paymentService.js
+++ FrontEnd/src/services/paymentService.js
@@ -13,8 +13,11 @@ const postPayment = (amount, phone, address, bankCode, language, cart) => {
 const getAllPayment = () => {
   return instance.get('payment/getAllPayment');
 };
+const getAllPaymentByStatus = () => {
+  return instance.get('payment/payment_history_succeed');
+};
 const updatePayment = (data) => {
   return instance.post('payment/update_state', data);
 };
 
-export { postPayment, getAllPayment, updatePayment };
+export { postPayment, getAllPayment, updatePayment, getAllPaymentByStatus };
