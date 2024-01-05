b1f0296f9bdbb071b4bee039b332599bb2d3e912
kyhuyho <hkhuy220802@gmail.com>
2024-01-05 21:23:08 +0700

    fix-product-quantity

--- FrontEnd/src/app/(default)/(public)/product/[id]/page.jsx
+++ FrontEnd/src/app/(default)/(public)/product/[id]/page.jsx
@@ -173,6 +173,10 @@ const ProductDetail = () => {
       );
       return;
     }
+    if (+book.quantity < count) {
+      toast.warning('Product quantity is not enough');
+      return;
+    }
     sessionStorage.setItem('priceBook', Number(price * count));
     sessionStorage.setItem('count', Number(count));
     sessionStorage.setItem('idBook', book?._id);
